cf33b3d7b8b0c2e8416946a00bfb274ccf6fd7d7
Sam Claughan <sam@highrise.digital>
2017-09-28 18:51:30 +0100

    Get your own salt.

--- wp-config.php
+++ wp-config.php
@@ -21,16 +21,19 @@ define( 'DB_COLLATE', '' );
 
 /**
  * Salts, for security - https://api.wordpress.org/secret-key/1.1/salt
+ *
+ * Go to that URL and get some for below.
  */
 
-define( 'AUTH_KEY', '3}Pjw.]k#l{GL)CMU97N67NCXv_F-Hk.A<4iV2W+JL#tv[t(R|6W,_Q3W.A]ziS~' );
-define( 'SECURE_AUTH_KEY', 'WmB%%9vfc(L5MH D)pe48Lc3mXey.tZ9o;vauTt3U{-DRR1w-zM&^gd@T);qzIS)' );
-define( 'LOGGED_IN_KEY', '{|9P{:J<xyQ`O28R@)]C0YKv=tDl|Wk-tAQP)paILTxE;4/,?OF]R63d+%ak |y$' );
-define( 'NONCE_KEY', ',T6F?>apCG_6l!TJ-p?!5/A#[/ DAivS+aG{]ZrNsDt)%<rin2W,ga$e]:@y&m-Z' );
-define( 'AUTH_SALT', 'ANT^C|2f|f%FqQvspc_+`n |yIeP{l)|lN5Lo;f>G:EhFS`4@U[-.?sWWiL2$c?n' );
-define( 'SECURE_AUTH_SALT', 'r5k^o;}i$3H mjfm+9 n/wvGE+3r+hkucyEC,k,pCne(gnM5rBO:adA{J$G<N$xI' );
-define( 'LOGGED_IN_SALT', 'bU?sdTUeVerUIk0iy1K-B8wS3vDyhJe#GPF.sO+X+QGC.$NlLIc$f3Y)E*E+YaUQ' );
-define( 'NONCE_SALT', '?P1Z*sVTCM((v>X@$/LY~Q{:#p1CaKQW>m6Qo.D@.oLN-g^B`<,#?x2RUAev=WJm' );
+define( 'AUTH_KEY', '' );
+define( 'SECURE_AUTH_KEY', '' );
+define( 'LOGGED_IN_KEY', '' );
+define( 'NONCE_KEY', '' );
+define( 'AUTH_SALT', '' );
+define( 'SECURE_AUTH_SALT', '' );
+define( 'LOGGED_IN_SALT', '' );
+define( 'NONCE_SALT', '' );
+
 /**
  * Table prefix
  */
